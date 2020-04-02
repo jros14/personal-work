85e6e2880cad8d9a32cd988ec9ee5c5564b5d07a
jrosenberg <julian.rosenberg@aperiatech.com>
2020-04-01 17:14:47 -0700

    Got the plots working!

--- Quanta Vibration Data Reader.py
+++ Quanta Vibration Data Reader.py
@@ -23,69 +23,23 @@ def plot_data(quanta_df, column_name):
 # def find_column():
 
 
-input1 = input('Which input would you like to view? Type a number or "control"')
-input1 = input1.lower()
-minute = int(input('What minute would you like to view the data from?'))
+#input1 = input('Which input would you like to view? Type a number or "control"')
+#input1 = input1.lower()
+#minute = int(input('What minute would you like to view the data from?'))
 quanta_data_path = r"C:\Users\jrosenburg\Desktop\Mom's Spaghetti\Learning\Python Studying\Sample Quanta Data\Durability Runs 5-8\06 Dura 15  30min.xlsx"
 df = pd.read_excel(quanta_data_path, sheet_name=0, header=0, index_col=0)
 df = df.drop(df.index[[0, 1]])
-df.columns = df.columns.str.strip()
-print("printing df....")
-print(df)
-if input1 != 'control':
-    input1 = "Z=" + str(minute) + "(EWaterfall_input" + input1 + "(f))"
-else:
-    input1 = "Z=" + str(minute) + "(EWaterfall_control(f))"
-print("Input1    :" + input1)
-x = df.loc[input1]
-print("printing x...")
-print(x)
-#plot_data(df, input1)
+df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('_ewaterfall_','').str.replace('z=','').str.replace('f','')
+column_to_plot = df['2control']
+y_scale_max = column_to_plot.max() + 1
+peak_resonance_frequency = pd.to_numeric(column_to_plot).idxmax()
 
-
-
-
-
-"""quanta_columns = quanta_data_original.columns.values.tolist()
-for column_text in quanta_columns:
-    number_to_check = column_text[2:5]
-    if (number_to_check[2] == ')'):
-        number_to_check[2] = ' '
-    #    print(column_text[2:5])
-    if number_to_check.strip() == str(minute):
-        print("It's minute " + str(minute) + "!!")
-        break"""
-
-#
-
-
-"""
-
-print("Start *******************************")
-for column_text in quanta_columns:
-    number_to_check = column_text[2:5]
-    if(number_to_check[2] == ')'):
-        number_to_check[2] = ' '
-#    print(column_text[2:5])
-    if number_to_check.strip() == str(minute):
-        print("It's minute " + str(minute) + "!!")
-        break
-y_column_name = "Z=" + minute + " (EWaterfall_control(f))"
-y_column = quanta_df.filter(regex=y_column_name, axis=1)   <============**********
-print(y_column_name)
-print(y_column)"""
-
-"""x_axis = quanta_df.index.values.tolist()
-x_axis = x_axis[2:]
-y_column = "Z=" + minute + " (EWaterfall_control(f))"
-print("Y Column = " + y_column)
-#print(str(type(x_axis)))
-#print("X Axis = " + str(x_axis))
-y_axis = quanta_df.loc[:, y_column]
-print("Y Axis = " + y_axis)
-
-plt.plot(x_axis, y_axis)
-plt.xlabel('Frequency')
+column_to_plot.plot()
+plt.yscale('log')
+plt.xscale('log')
+plt.axis([1, 1125, .00001, y_scale_max])
+plt.xlabel('Frequency      Peak Resonance @ ' + str(peak_resonance_frequency) + " Hz")
 plt.ylabel('G^2/Hz')
+plt.tight_layout()
 plt.title("PSD")
-plt.show()"""
+plt.show()
