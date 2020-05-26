1246b8c8830d20c8af69c940960a8ac1c3dfa678
Julian Rosenberg <jrosenberg@MacBook-Pro.local>
2020-05-25 21:41:19 -0700

    working on building next grid that contains all the updated values

--- Game of Life/GameOfLife.py
+++ Game of Life/GameOfLife.py
@@ -5,28 +5,31 @@ import random
 class Grid:
     def __init__(self):
         self.grid = []
-        self.length = 40
+        self.next_grid = []
 
     def create_random_grid(self, grid_dimension=None):
         if grid_dimension is None:
-            self.length = 40
+            length = 40    # default grid size
         else:
-            self.length = grid_dimension
+            length = grid_dimension
 
-        for row in range(self.length):
-            self.grid.append(random.choices(["o", "_"], k=self.length))
+        for row in range(length):
+            self.grid.append(random.choices(["o", "_"], k=length))
+        self.display_grid()
+
+    def display_grid(self):
         for row in self.grid:
             for cell in row:
                 print(cell, end=" ")
             print()
-
-        return self.grid
 
     def neighboring_cells(self, cell_location):
         x_value = cell_location[0]
         y_value = cell_location[1]
         num_o_neighbors = 0
         num_blank_neighbors = 0
+
+        #TODO: Figure out how to deal with the edge numbers without making it all just a bunch of annoying if statements
 
         # surrounding neighbors in list form. E.g. for cell [1,1]: [[0,0] [0,1] [0,2] [1,0] [2,0] [2,1] [2,2] [1,2]]
         neighbors_loc = [[x_value-1, y_value-1], [x_value-1, y_value], [x_value-1, y_value+1], [x_value, y_value-1],
@@ -40,8 +43,20 @@ class Grid:
 
         return num_o_neighbors, num_blank_neighbors
 
-    def get_cell_value(self, cell_location):
+    def change_cell_value(self, cell_location):
         if self.grid[cell_location[0]][cell_location[1]] == "o":
-            return True
+            self.grid[cell_location[0]][cell_location[1]] = "_"
         else:
-            return False
+            self.grid[cell_location[0]][cell_location[1]] = "o"
+
+    def build_next_grid(self):
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid)):
+                num_o_neighbors, num_blank_neighbors = self.neighboring_cells([row, column])
+                if num_o_neighbors < 2:
+                    self.next_grid[row][column] = "_"
+                elif (num_o_neighbors == 2 or num_o_neighbors == 3) and self.grid[row][column] == "o":
+                    self.next_grid[row][column] = "o"
+                else:
+                    self.next_grid[row][column] = "o"
+        print(self.next_grid)
--- Game of Life/test_gameoflife.py
+++ Game of Life/test_gameoflife.py
@@ -2,43 +2,47 @@ import unittest
 from GameOfLife import Grid
 
 
+def setup_grid():
+    grid_object = Grid()
+    grid = [["o", "o", "_", "o"],
+            ["o", "_", "_", "o"],
+            ["o", "o", "_", "o"],
+            ["o", "o", "_", "o"]]
+    grid_object.grid = grid
+    return grid_object, grid
+
+
 class TestGameOfLife(unittest.TestCase):
 
     def test_creates_random_grid(self):
         grid_object = Grid()
         grid_size = 10
-        grid = grid_object.create_random_grid(grid_size)
+        grid_object.create_random_grid(grid_size)
+        grid = grid_object.grid
         rows = len(grid)
         columns = len(grid[0])
         self.assertEqual(grid_size, rows, "Not creating correct number of rows")
         self.assertEqual(grid_size, columns, "Not creating correct number of columns")
 
     def test_getting_value_of_neighboring_cells(self):
-        grid_object, grid = self.setup_grid()
+        grid_object, grid = setup_grid()
         cell = [1, 1]
 
         num_o, num_blank = grid_object.neighboring_cells(cell)
         self.assertEqual(5, num_o, "not gathering neighboring cells correctly")
         self.assertEqual(3, num_blank, "not gathering neighboring cells correctly")
 
-    def test_getting_cell_value(self):
-        grid_object = Grid()
+    def test_change_cell_value(self):
+        grid_object, grid = setup_grid()
         cell = [1, 1]
-        grid = self.setup_grid()
-        grid_object.grid = grid
 
-        # keep working from here
-        self.assertFalse()
+        grid_object.change_cell_value(cell)
+        self.assertEqual("o", grid[1][1], "cells not switching value")
 
-    def setup_grid(self):
-        grid_object = Grid()
-        grid =  [["o", "o", "_", "o"],
-                ["o", "_", "_", "o"],
-                ["o", "o", "_", "o"],
-                ["o", "o", "_", "o"]]
-        grid_object.grid = grid
-        return grid_object, grid
-
+    def test_build_new_grid(self):
+        grid_object, grid = setup_grid()
+        grid_object.build_next_grid()
+        self.assertEqual("o", grid_object.next_grid[1][7])
 
 if __name__ == '__main__':
     unittest.main()
